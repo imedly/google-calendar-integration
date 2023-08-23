70ad90fedfedc900306f2d4f22482f757f0cbc6b
M. Id <19668275+imedly@users.noreply.github.com>
2023-08-23 22:50:31 +0100

    Validation Google ID Auth Credentials

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ const app = express()
 
 const PORT = process.env.NODE_ENV || 8000
 
-app.get('/google', (req))
+const auth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+)
+
+app.get('/google', (req, res) => {
+const url = 
+});
 
 app.listen(PORT, () =>{console.log("Server startd on port : ", PORT);});
 
