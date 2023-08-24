8ab6f3b7d463da9da36ec70235fdf15fa2fd3a7d
M. Id <19668275+imedly@users.noreply.github.com>
2023-08-24 09:25:01 +0100

    Test Working Google Cloud Redirect

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import express from 'express'
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+import express from "express";
+import { google } from "googleapis";
 
-const PORT = process.env.NODE_ENV || 8000
+const app = express();
 
-const auth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URL
-)
+const PORT = process.env.PORT || 8000; // Corrected process.env.PORT
 
-app.get('/google', (req, res) => {
-const url = 
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+
+const scopes = ["https://www.googleapis.com/auth/calendar"];
+
+app.get("/google", (req, res) => {
+    const url = oauth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: scopes,
+      redirect_uri: process.env.REDIRECT_URL, // Spécifiez explicitement le redirect_uri ici
+    });
+    res.redirect(url);
+  });
+  
+
+// Corrected parameter order: (req, res)
+app.get("/google/redirect", (req, res) => {
+  res.send("It's working");
 });
 
-app.listen(PORT, () =>{console.log("Server startd on port : ", PORT);});
-
+app.listen(PORT, () => {
+  console.log("Server started on port:", PORT);
+});
